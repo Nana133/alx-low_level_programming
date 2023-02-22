8ad491768c53f1969fb05cc81641ce9cefe34f91
Nana133 <you@example.com>
2023-02-22 02:36:54 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "main.h"
-/
+/**
+ *
  * _islower-check if character is lowercase.
- * @c:character to check if it islowercase.
+ * '@'c:character to check if it islowercase.
  * Return:0-if lowercase,1-if not.
  */
 
