d7e742baee3ee6beb4faa184ef8329d71c29dc64
Nana133 <you@example.com>
2023-03-08 02:18:18 -0800

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,33 +1,21 @@
-#ifndef main.h
-#define main.h
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x07-recursion directory.
+ */
 
 int _putchar(char c);
-
-/* 0-puts_recursion.c */
 void _puts_recursion(char *s);
-
-/* 1-print_rev_recursion.c */
 void _print_rev_recursion(char *s);
-
-/* 2-strlen_recursion.c */
 int _strlen_recursion(char *s);
-
-/* 3-factorial.c */
 int factorial(int n);
-
-/* 4-pow_recursion.c */
 int _pow_recursion(int x, int y);
-
-/* 5-sqrt_recursion.c */
 int _sqrt_recursion(int n);
-
-/* 6-is_prime_number.c */
 int is_prime_number(int n);
-
-/* 7-is_palindrome.c */
 int is_palindrome(char *s);
-
-/* 100-wildcmp.c */
 int wildcmp(char *s1, char *s2);
 
 #endif
