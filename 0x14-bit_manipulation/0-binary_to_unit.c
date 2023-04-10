f157bc6ca1df119ecbb816fbecb2280c98918aa5
Nana133 <odosaint@gmail.com>
2023-04-10 15:21:38 -0700

    0-binary_to_unit.c

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,41 +1,21 @@
 #include "main.h"
 /**
- * binary_to_uint - converts a binary to an unsigned int
- * @b: the binary number as a string
+ * binary_to_uint - To convert a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Return: the converted value
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int decimal = 0;
-int str_len = 0, base = 1;
-if (!check_valid_string(b))
-return (0);
-while (b[str_len] != '\0')
-str_len++;
-while (str_len)
-{
-decimal += ((b[str_len - 1] - '0') * base);
-base *= 2;
-str_len--;
-}
-return (decimal);
-}
-/**
- * check_valid_string - checks if a string has only 0's and 1's
- * @b: string to be checked
- *
- * Return: 1 if string is valid, 0 otherwise
- */
-int check_valid_string(const char *b)
-{
-if (b == NULL)
-return (0);
-while (*b)
-{
-if (*b != '1' && *b != '0')
-return (0);
-b++;
-}
-return (1);
+	int u;
+	unsigned int dec_val = 0;
+	if (!b)
+		return (0);
+	for (u = 0; b[u]; u++)
+	{
+		if (b[u] < '0' ||b[u] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[u] - '0');
+	}
+	return (dec_val);
 }
