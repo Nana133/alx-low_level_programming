37defe01b4767e393b889cae1758ee840166f1e8
Nana133 <odosaint@gmail.com>
2023-05-08 14:47:36 -0700

    updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 
 /*
- * Desc: Header file contains prototypes of functions written in 0x14-file_io directory.
+ * Desc: Header file contains prototypes of all functions
+ *       written in 0x14-file_io directory.
  */
 
 #include <sys/types.h>
@@ -10,10 +11,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int create_file(const char *filename, char *text_c
-ontent);
-ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
