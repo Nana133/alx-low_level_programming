09345ba3296a7b49a0e912f3a8cef72034ba96e1
Nana133 <odosaint@gmail.com>
2023-05-08 14:58:03 -0700

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int a, b, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,13 +22,13 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	b = write(a, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (a == -1 || b == -1)
 		return (-1);
 
-	close(o);
+	close(a);
 
 	return (1);
 }
